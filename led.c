e400d3018a646165a68924d88efc35e52daeec9b
hhj779 <1430904946@qq.com>
2024-05-23 22:30:39 +0800

    修改结果显示

--- led.c
+++ led.c
@@ -10,13 +10,13 @@
 // 0-9 无显示 11左12右括号13加14减15乘16除
 code unsigned char NT[] = {0x08, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x3F, 0x39, 0x0f, 0x70, 0x40, 0x80, 0x46};
 #define Length 24
-unsigned char show_length       = 20;  // 显示长度
+unsigned char show_length       = 1;  // 显示长度
 int show_mode                   = -1; // 显示模式，0是正常显示模式，1是修改模式  -1是输入模式
 unsigned char Scintillation_bit = 0;  // 闪烁位
 bit Scintillation_flag          = 0;  // 0表示闪烁位灭，1表示闪烁位亮
 long int answer                      = 49154;
-// unsigned char Number[Length] = {10, 11, 12, 13, 14, 15, 16, 7, 8, 9, 1, 1, 2, 3, 4, 5,0,0,0,0,0,0,0,0};
-unsigned char Number[Length] = {1, 2, 3, 15,11, 4, 2, 5, 14, 6, 2, 8, 16, 3, 2, 12, 14, 7, 8, 4};
+//unsigned char Number[Length] = {1, 2, 3, 15,11, 4, 2, 5, 14, 6, 2, 8, 16, 3, 2, 12, 14, 7, 8, 4};
+unsigned char Number[Length]={0};
 // 123*(425-628/32)-784
 sbit P2_2             = P2 ^ 2;
 sbit P2_3             = P2 ^ 3;
@@ -123,7 +123,7 @@ void Task_2(void) _task_ 2
             } else if (KeyNum == Independent_button_4) { // 独立按键4
                                                          // 计算
                    //answer=ans_calculate(Number);
-										answer              = 49154;
+					answer              = 49154;
                    for(count=0;count<Length;count++)
                    {
                        Number[count]=0;
